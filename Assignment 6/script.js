d2b8f54b7b30567cf098c4c6eb9a76cf02a5e10e
Mahendras1ngh <iammahendrasingh@hotmail.com>
2021-03-28 13:57:52 +0530

    css done

--- Assignment 6/script.js
+++ Assignment 6/script.js
@@ -1,7 +1,8 @@
 const date = document.querySelector("#date");
 const input = document.querySelector("#input");
 const addTaskBtn = document.querySelector("#addTaskBtn");
-const tasks = document.querySelector("#tasks")
+const tasks = document.querySelector("#tasks");
+const btndiv= document.querySelector("#btndiv");
 
 // Formatting date
 var objToday = new Date(),
@@ -29,58 +30,74 @@ const addtask=()=>{
         if(newCheckBox.checked==true){
             const currentlabel = newCheckBox.nextSibling;
             currentlabel.style.textDecoration = "line-through";
+            btndiv.style.display ="none";
         }
         else{
             const currentlabel = newCheckBox.nextSibling;
             currentlabel.style.textDecoration = "none";
+            btndiv.style.display ="inline-block";
         }
     }
     
 
     const newDiv = document.createElement("div");
+    newDiv.setAttribute("class","task");
 
     const newPara = document.createElement("p");
     newPara.textContent= input.value;
 
+    const btndiv=document.createElement("div");
+    btndiv.setAttribute("id","btndiv");
+
     const newEditBtn = document.createElement("button");
-    newEditBtn.textContent= "Edit";
-    newEditBtn.addEventListener("click",editTask);
+    // newEditBtn.textContent= "Edit";
+
+    const newimg= document.createElement("img");
+    newimg.setAttribute("src","icons/icons8-edit-24.png");
+    newimg.addEventListener("click",editTask);
 
     const newDeleteBtn = document.createElement("button");
-    newDeleteBtn.textContent= "Delete";
-    newDeleteBtn.addEventListener("click",(e)=>e.target.parentElement.remove());
+    // newDeleteBtn.textContent= "Delete";
+    newDeleteBtn.addEventListener("click",(e)=>e.target.parentElement.parentElement.parentElement.remove());
+
+    const newimg2= document.createElement("img");
+    newimg2.setAttribute("src","icons/icons8-trash-24.png");
 
     tasks.appendChild(newDiv);
     newDiv.appendChild(newCheckBox);
     newDiv.appendChild(newPara);
-    newDiv.appendChild(newEditBtn);
-    newDiv.appendChild(newDeleteBtn);
+    newDiv.appendChild(btndiv);
+    btndiv.appendChild(newEditBtn);
+    newEditBtn.appendChild(newimg);
+    newDeleteBtn.appendChild(newimg2);
+    btndiv.appendChild(newDeleteBtn);
 
     input.value="";
 
 }
 
 addTaskBtn.addEventListener("click",addtask);
-console.log(addTaskBtn);
+// console.log(addTaskBtn);
 
 const editTask= (e)=>{
-    const Ctask= e.target.previousSibling;
+    const Ctask= e.target.parentElement.parentElement.previousSibling;
     const update = (e) => {
         Ctask.textContent = input.value;
         addTaskBtn.textContent="Add Task";
         addTaskBtn.removeEventListener("click",update);
         addTaskBtn.addEventListener("click",addtask);
-        console.log(Ctask);
-        console.log(addTaskBtn);
+        // console.log(Ctask);
+        // console.log(addTaskBtn);
         input.value="";
     }
 
+    console.dir(e.target.parentElement);
+    console.log(addTaskBtn);
     input.value=Ctask.textContent;
     addTaskBtn.textContent="Update";
     addTaskBtn.removeEventListener("click",addtask);
     addTaskBtn.addEventListener("click",update);
-    console.log(Ctask);
-    console.log(addTaskBtn);
+    
 }
 
 
